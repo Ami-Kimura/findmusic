45e47622a2bbc4eede502ad2fd21b32b67a3f72e
Ami-Kimura <82437518+Ami-Kimura@users.noreply.github.com>
2022-10-25 14:21:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function() {
     // 実行したい処理を書く
  
 
-fetch("https://accounts.spotify.com/authorize?client_id=7abf6edbf6a9478dbd3ae9afaed92dea&response_type=code&redirect_uri=http://localhost:8888")
+fetch("https://accounts.spotify.com/authorize?client_id=7abf6edbf6a9478dbd3ae9afaed92dea&response_type=code&redirect_uri=https://ami-kimura.github.io/findmusic/")
   .then(response => {
     return response.json();
   })
